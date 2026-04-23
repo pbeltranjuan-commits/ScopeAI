b0f24126871c150656166daa91119ef50d14f9ce
pbeltranjuan-commits <pbeltranjuan@gmail.com>
2026-04-23 15:22:01 +0200

    Add files via upload

---
+++ ingenieria.py
@@ -0,0 +1,20 @@
+# ingenieria.py
+
+def obtener_manual_formulas():
+    return """
+    MANUAL TÈCNIC SCOPEAI:
+    
+    1. HIDRÀULICA:
+       - Hazen-Williams: h_f = 10.67 * (Q^1.852) / (C^1.852 * D^4.87)
+       - Darcy-Weisbach: h_f = f * (L/D) * (v^2 / 2g)
+       
+    2. ELECTRICITAT:
+       - Llei d'Ohm: V = I * R
+       - Potència: P = V * I
+       
+    3. TERMODINÀMICA:
+       - Càrrega tèrmica: Q = m * Cp * ΔT
+       
+    4. ESTRUCTURES:
+       - Tensió: σ = F / A
+    """
